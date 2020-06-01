9d0e3ef8e6e23c7effee89067bf3ef9603348232
Us Nikolay <mr.us.nick@gmail.com>
2020-06-01 17:34:07 +0300

    Доделан МГТУ_ГА забыл файлы кинуть Подключаем PDFBox для работы с приказами остальных вузов

---
+++ src/WooHoo.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class WooHoo {
+            public static void main(String[] args) throws Exception {
+
+                BufferedReader r = new BufferedReader(new InputStreamReader(new FileInputStream("C:\\US_NEW\\1.txt")));
+
+                InputStreamReader inputStreamReader = new InputStreamReader(new FileInputStream("C:\\US_NEW\\1.txt"));
+
+//                System.out.println( r.readLine());
+                int k = 0;
+                while ((k=Integer.parseInt(r.readLine())) > 0) {
+                    System.out.println(k);
+
+                }
+
+
+
+
+                int result = 0;
+//        for (int i = 0; i < S.length(); ++i) {
+//
+//            char ch = S.charAt(i);
+//            for (int j = 0; j < J.length();j++ ){
+//                if (ch==J.charAt(j))++result;
+//
+//            }
+//
+//        }
+//        while(System.in.)
+//        String S = r.readLine();
+//        char[] arrays = new char[S.length()];
+//        arrays = S.toCharArray();
+//        for (char array : arrays) {
+//            System.out.println(array);
+//
+//        }
+
+//        System.out.println(x);
+//        int i, N = 0, m=0, n0, n1 = 0;
+//
+//        while ((n0 = inputStreamReader.read())>=0) {
+//            System.out.println(n0);
+//            n1=inputStreamReader.read();
+//            if(n0==49 & n1 ==49)m++;
+//
+//            if (n1 ==10 && n0 ==13){
+//                if (m>N)N = m;
+//                m=0;
+//
+//                }
+//            }
+
+
+//        System.out.println("********** "+N);
+    }
+    }
